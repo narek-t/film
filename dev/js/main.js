55d8559eb6ad7aaf84ab022d08f7477643e9573b
Narek <narek.tarverdyan@bk.ru>
2017-02-22 16:15:15 +0300

    pages+

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,4 +4,37 @@ $(document).ready(function() {
 		dots: true,
 	});
 	$('.home-slider__trailer').slick();
+
+	$('.makers__list').magnificPopup({
+		fixedContentPos: true,
+		fixedBgPos: true,
+		overflowY: 'auto',
+		mainClass: 'act-popup',
+		delegate: 'a',
+		gallery: {
+			enabled: true
+		},
+	});
+
+
+	$('.single-slider__thumbs').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1
+	});
+
+
+
+	$('.single-slider__thumbs .img-wrap').click(function(event) {
+		var src = $(this).data('src');
+		var videoUrl = $(this).data('url');
+		if(src) {
+			$(this).parents('.act-slider').find('.single-slider__view').css('background-image', 'url('+src+')');
+			$(this).parents('.act-slider').find('.single-video').attr('src', '');
+		} else {
+			$(this).parents('.act-slider').find('.single-video').attr('src', videoUrl);
+		}
+	});
+
+
 });
