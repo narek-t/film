eb2314dbd8719f16afee562e6e541a2ab0189760
Narek-T <narek.tarverdyan@bk.ru>
2017-03-01 11:42:08 +0300

    announcement page +

--- dev/js/main.js
+++ dev/js/main.js
@@ -75,5 +75,13 @@ $(document).ready(function() {
 		},
 	});
 
+	$('.calendar__item-top').click(function(event) {
+		$(this).parents('.calendar__list-item').toggleClass('active');
+		google.maps.event.trigger(map2, 'resize');
+		google.maps.event.trigger(map3, 'resize');
+		google.maps.event.trigger(map4, 'resize');
+		google.maps.event.trigger(map5, 'resize');
+	});
+
 
 });
